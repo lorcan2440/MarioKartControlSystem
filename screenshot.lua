5fd4b55d04b2bee4e86679625583dc20681ded08
Lorcan <72615977+lorcan2440@users.noreply.github.com>
2024-06-10 15:53:18 +0100

    Update screenshot.lua

--- screenshot.lua
+++ screenshot.lua
@@ -14,6 +14,7 @@ local mime = require("mime")
 local host, port = "127.0.0.1", 12345
 local client = assert(socket.connect(host, port))
 client:settimeout(0.1)  -- set 100 ms timeout for non-blocking mode
+print("Connected to the server: " .. host .. ":" .. port)
 
 OR, XOR, AND = 1, 3, 4
 
@@ -28,7 +29,7 @@ function bitoper(a, b, oper)
 end
 
 function sendScreenshot()
-    local screen = gui.gdscreenshot()
+    local screen = gui.gdscreenshot{whichScreen="bottom"}
     local img = gd.createFromGdStr(screen)
     local pngData = img:pngStr()
     local dataLength = string.format("%09d", #pngData)
@@ -36,26 +37,43 @@ function sendScreenshot()
     client:send(pngData)
 end
 
-function receiveButtons(client)
+function receiveButtons()
     local data, err = client:receive(1)  -- 1 byte from the Python script
     if data then
         local buttons = string.byte(data)
         -- decode the buttons
         -- A: 128, Left: 64, Right: 32, Unused: 16, 8, 4, 2, 1
-        local A_press = bitoper(buttons, 128, AND) == 128
-        local left_press = bitoper(buttons, 64, AND) == 64
-        local right_press = bitoper(buttons, 32, AND) == 32
-        local buttons_table = {A = A_press, left = left_press, right = right_press}
+        local A_press = bitoper(buttons, 128, AND) ~= 0
+        local left_press = bitoper(buttons, 64, AND) ~= 0
+        local right_press = bitoper(buttons, 32, AND) ~= 0
+        -- only actively set left and right to pressed if they are  received
+        -- this allows manual override while driving
+        local buttons_table = {}
+        if left_press then
+            buttons_table = {A = A_press, left = left_press}
+        elseif right_press then
+            buttons_table = {A = A_press, right = right_press}
+        else
+            buttons_table = {A = A_press}
+        end
         joypad.set(1, buttons_table)
+        -- other useful functions for allowing manual override:
+        -- joypad.get(), joypad.read(), joypad.peek(), joypad.set()
+        -- can also request which buttons are 'only pressed' (down) or 'only released' (up)
     else
+        -- socket is closed
         print("Error receiving data:", err)
+        print("Disconnecting from server and pausing the game.")
+        client:close()
+        emu.pause()
     end
 end
 
-while not input.get().x do  -- press 'q' to stop streaming
+while not input.get().Q do  -- press key 'q' while on DeSmuME window to stop streaming
     sendScreenshot()
-    receiveButtons(client)
+    receiveButtons()
     emu.frameadvance()
 end
 
+print("Quitting and disconnecting from the server.")
 client:close()
